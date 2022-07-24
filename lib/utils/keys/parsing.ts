94144dfaff274b5d225c6750af73b29a34d601f7
Rida F'kih <hello@rida.dev>
2022-07-23 22:28:42 -0600

    improve `isTypeOfKey` prediicate, abstract to parsing util

---
+++ lib/utils/keys/parsing.ts
@@ -0,0 +1,29 @@
+import type { PrivateKey, PublicKey, SomeKey } from "typings/key-types";
+
+const parseKey = (candidate: string) => {
+  const [type, key] = candidate.split("$") as (string | undefined)[];
+  return { type, key };
+};
+
+const isValidKey = (candidate: string): candidate is SomeKey => {
+  const { type, key } = parseKey(candidate);
+
+  if (!type || !key) return false;
+  if (key.length !== 44) return false;
+  return type === "public" || type === "private";
+};
+
+export const isTypeOfKey = <T extends "public" | "private">(
+  type: T,
+  candidate: SomeKey
+): candidate is T extends "private"
+  ? PrivateKey
+  : T extends "public"
+  ? PublicKey
+  : unknown => {
+  const isKeyValid = isValidKey(candidate);
+  if (!isKeyValid) return false;
+
+  const parsedKey = parseKey(candidate);
+  return parsedKey.type === type;
+};
