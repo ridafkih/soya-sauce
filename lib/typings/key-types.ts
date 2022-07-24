9e10ff8f881d015da0638ddf38dfcef30db08d91
Rida F'kih <hello@rida.dev>
2022-07-23 22:28:47 -0600

    add keypair object typ

--- lib/typings/key-types.ts
+++ lib/typings/key-types.ts
@@ -2,5 +2,9 @@ type KeyString = string[44];
 
 export type PrivateKey = `private$${KeyString}`;
 export type PublicKey = `public$${KeyString}`;
-
 export type SomeKey = PrivateKey | PublicKey;
+
+export type KeyPair = {
+  private: PrivateKey;
+  public: PublicKey;
+};
