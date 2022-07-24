f709ff24be010953823f75f53f7f2ae02cc08fb4
Rida F'kih <hello@rida.dev>
2022-07-24 14:16:42 -0600

    fix coverage

--- lib/test/modules/secret-box/initialization.test.ts
+++ lib/test/modules/secret-box/initialization.test.ts
@@ -3,34 +3,32 @@ import { test, expect, it } from "vitest";
 import { SecretBox } from "modules/secret-box";
 import { Errors } from "typings/errors";
 
-test("initialized `SecretBox` cannot be initialized again", () => {
-  const MASTER_KEY = "";
+const MASTER_KEY = "";
 
-  it("should throw when `withoutMasterKey` is called twice", () => {
-    const initialize = () =>
-      new SecretBox().withoutMasterKey().withoutMasterKey();
+test("initialized `SecretBox` throw when `withoutMasterKey` is called twice", () => {
+  const initialize = () =>
+    new SecretBox().withoutMasterKey().withoutMasterKey();
 
-    expect(initialize).rejects.toThrow(Errors.ALREADY_INITIALIZED);
-  });
+  expect(initialize).toThrow(Errors.ALREADY_INITIALIZED);
+});
 
-  it("should throw when `withMasterKey` is called twice", () => {
-    const initialize = () =>
-      new SecretBox().withMasterKey(MASTER_KEY).withMasterKey(MASTER_KEY);
+test("initialized `SecretBox` throw when `withMasterKey` is called twice", () => {
+  const initialize = () =>
+    new SecretBox().withMasterKey(MASTER_KEY).withMasterKey(MASTER_KEY);
 
-    expect(initialize).rejects.toThrow(Errors.ALREADY_INITIALIZED);
-  });
+  expect(initialize).toThrow(Errors.ALREADY_INITIALIZED);
+});
 
-  it("should throw when `withMasterKey` is called after `withoutMasterKey`", () => {
-    const initialize = () =>
-      new SecretBox().withoutMasterKey().withMasterKey(MASTER_KEY);
+test("initialized `SecretBox` throw when `withMasterKey` is called after `withoutMasterKey`", () => {
+  const initialize = () =>
+    new SecretBox().withoutMasterKey().withMasterKey(MASTER_KEY);
 
-    expect(initialize).rejects.toThrow(Errors.ALREADY_INITIALIZED);
-  });
+  expect(initialize).toThrow(Errors.ALREADY_INITIALIZED);
+});
 
-  it("should throw when `withoutMasterKey` is called after `withMasterKey`", () => {
-    const initialize = () =>
-      new SecretBox().withMasterKey(MASTER_KEY).withoutMasterKey();
+test("initialized `SecretBox` throw when `withoutMasterKey` is called after `withMasterKey`", () => {
+  const initialize = () =>
+    new SecretBox().withMasterKey(MASTER_KEY).withoutMasterKey();
 
-    expect(initialize).rejects.toThrow(Errors.ALREADY_INITIALIZED);
-  });
+  expect(initialize).toThrow(Errors.ALREADY_INITIALIZED);
 });
