069848f3a54496d0fb67e7cb390fb362e1ccbcb8
Rida F'kih <hello@rida.dev>
2022-07-24 14:04:31 -0600

    add secret-box initialization tests

---
+++ lib/test/modules/secret-box/initialization.test.ts
@@ -0,0 +1,37 @@
+import { test, expect, it } from "vitest";
+
+import { SecretBox } from "modules/secret-box";
+import { Errors } from "typings/errors";
+
+test("initialized `SecretBox` cannot be initialized again", () => {
+  it("should throw when `withoutMasterKey` is called twice", () => {
+    const initialize = () =>
+      new SecretBox().withoutMasterKey().withoutMasterKey();
+
+    expect(initialize).rejects.toThrow(Errors.ALREADY_INITIALIZED);
+  });
+
+  it("should throw when `withMasterKey` is called twice", () => {
+    const MASTER_KEY = "";
+    const initialize = () =>
+      new SecretBox().withMasterKey(MASTER_KEY).withMasterKey(MASTER_KEY);
+
+    expect(initialize).rejects.toThrow(Errors.ALREADY_INITIALIZED);
+  });
+
+  it("should throw when `withMasterKey` is called after `withoutMasterKey`", () => {
+    const MASTER_KEY = "";
+    const initialize = () =>
+      new SecretBox().withoutMasterKey().withMasterKey(MASTER_KEY);
+
+    expect(initialize).rejects.toThrow(Errors.ALREADY_INITIALIZED);
+  });
+
+  it("should throw when `withoutMasterKey` is called after `withMasterKey`", () => {
+    const MASTER_KEY = "";
+    const initialize = () =>
+      new SecretBox().withMasterKey(MASTER_KEY).withoutMasterKey();
+
+    expect(initialize).rejects.toThrow(Errors.ALREADY_INITIALIZED);
+  });
+});
