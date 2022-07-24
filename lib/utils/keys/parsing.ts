4057017f0f687590f5a04bdea53f0df7e3ce42ff
Rida F'kih <hello@rida.dev>
2022-07-23 22:52:57 -0600

    abstract parsing & validation for key utils

--- lib/utils/keys/parsing.ts
+++ lib/utils/keys/parsing.ts
@@ -1,32 +1,10 @@
+import { isValidKey } from "utils/keys/validation";
 import { Errors } from "typings/errors";
-import type { PrivateKey, PublicKey, SomeKey } from "typings/key-types";
+import type { SomeKey } from "typings/key-types";
 
-const parseKey = (candidate: string) => {
+export const parseKey = (candidate: string) => {
   const [type, key] = candidate.split("$") as (string | undefined)[];
   return { type, key };
-};
-
-const isValidKey = (candidate: string): candidate is SomeKey => {
-  const { type, key } = parseKey(candidate);
-
-  if (!type || !key) return false;
-  if (key.length !== 44) return false;
-  return type === "public" || type === "private";
-};
-
-export const isTypeOfKey = <T extends "public" | "private">(
-  type: T,
-  candidate: SomeKey
-): candidate is T extends "private"
-  ? PrivateKey
-  : T extends "public"
-  ? PublicKey
-  : unknown => {
-  const isKeyValid = isValidKey(candidate);
-  if (!isKeyValid) return false;
-
-  const parsedKey = parseKey(candidate);
-  return parsedKey.type === type;
 };
 
 export const getKeyBuffer = <T extends SomeKey>(candidate: T) => {
