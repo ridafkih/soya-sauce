f3bed533abb5937d45eb608a56767809c327c12c
Rida F'kih <hello@rida.dev>
2022-07-23 22:12:17 -0600

    prefix all constants

--- lib/constants/crypto.ts
+++ lib/constants/crypto.ts
@@ -1,7 +1,7 @@
-export const NONCEBYTES = 24;
+export const BOX_NONCEBYTES = 24;
 
-export const ALG_ARGON2ID13 = 2;
-export const MEMLIMIT_MODERATE = 268435456;
-export const OPSLIMIT_MODERATE = 3;
+export const HASH_ALG_ARGON2ID13 = 2;
+export const HASH_MEMLIMIT_MODERATE = 268435456;
+export const HASH_OPSLIMIT_MODERATE = 3;
 
-export const SALTBYTES = 16;
+export const HASH_SALTBYTES = 16;
